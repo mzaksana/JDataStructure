f77dbc1b96892d808c8e2b19a653e305eb73d5c7
Muammar Zikri Aksana <muammar.clasic@gmail.com>
2019-03-19 23:04:57 +0700

    edit to read line by line of file

--- data/ReadFile.java
+++ data/ReadFile.java
@@ -1,19 +1,14 @@
 package data;
 
-import java.io.BufferedReader;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
+import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
 public class ReadFile {
     private String namaFileSrc;
-    private BufferedReader reader;
     public List<String> file;
 
-    public ReadFile(){/**/}
     public ReadFile(String pathSrc){
         this.namaFileSrc=pathSrc;
         file = new ArrayList<>();
@@ -21,37 +16,15 @@ public class ReadFile {
     }
 
     public int init(){
-        String isi;
         try {
-            reader = new BufferedReader(new FileReader(this.namaFileSrc));
-            while ((isi = reader.readLine()) != null) {
-                Scanner s2 = new Scanner(isi);
-                while (s2.hasNext()) {
-                    String s = s2.next();
-                    try{
-                        //System.out.println(breakString(s));
-                        //System.out.println(s);
-//                        Proses proses = new Proses(s.substring(0,breakString(s)),Integer.parseInt(s.substring(breakString(s)+1)));
-                        file.add(s);
-                    }catch(Exception e){
-                        // data yang diambil hanya yang formatnya sesuai
-                        System.out.println("formatnya");
-                    }
-                }
-                s2.close();
+            Scanner reader = new Scanner(new File(this.namaFileSrc));
+            while (reader.hasNextLine()) {
+                file.add(reader.nextLine());
             }
-            return 1;
         } catch (FileNotFoundException e) {
             System.out.println("not found " + this.namaFileSrc + " in "+System.getProperty("user.dir"));
-        } catch (IOException e) {
-            e.printStackTrace();
         } finally {
-            try {
-                if (reader != null) {
-                    reader.close();
-                }
-            } catch (IOException e) {
-            }
+            System.out.println("error ReadFile");
         }
         return 0;
     }
