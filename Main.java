286eace736c9431869ebbbc93933b2fb3ff6d7be
Muammar Zikri Aksana <muammar.clasic@gmail.com>
2019-03-20 00:39:20 +0700

    edit main file
    tambah bagian untuk add list dan read list

--- Main.java
+++ Main.java
@@ -1,11 +1,23 @@
-import data.ReadFile;
+import data.ListHeadQuotes;
+import data.ListQuotes;
+import data.Quotes;
+import file_reader.ReadFile;
 
 public class Main {
     public static void main(String[] args) {
-        int a=0;
-         ReadFile readFile = new ReadFile("data/file.txt");
-         for (String line:readFile.getLine()) {
-             System.out.println("line "+(a++)+" : "+line);
-         }
+        ListHeadQuotes listHeadQuotes = new ListHeadQuotes();
+        ReadFile readFile = new ReadFile("data/file.txt");
+            for (String line:readFile.getLine()) {
+            String []parseLine = line.split("::");
+            ListQuotes listQuotes = new ListQuotes(new Quotes(parseLine[0]),listHeadQuotes.getHead());
+            listHeadQuotes.setHead(listQuotes);
+            //             System.out.println("line "+(a++)+" : "+line);
+        }
+
+        ListQuotes listQuotes1 = listHeadQuotes.getHead();
+        while (listQuotes1.hasNext()){
+            System.out.println(listQuotes1.getQuotes()+"\n");
+            listQuotes1=listQuotes1.getNext();
+        }
     }
 }
