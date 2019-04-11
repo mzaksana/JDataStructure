f4128279f48d1a02cb1d89dd1cd3fc0d003009bb
muammar zikri aksana <mza@localhost.localdomain>
2019-04-11 13:24:14 +0700

    new init

--- Main.java
+++ Main.java
@@ -1,5 +1,5 @@
-import data.ListHeadQuotes;
-import data.ListQuotes;
+import data.list.ListHeadQuotes;
+import data.list.ListQuotes;
 import data.Quotes;
 import file_reader.ReadFile;
 
@@ -7,15 +7,19 @@ public class Main {
     public static void main(String[] args) {
         ListHeadQuotes listHeadQuotes = new ListHeadQuotes();
         ReadFile readFile = new ReadFile("data/file.txt");
-            for (String line:readFile.getLine()) {
-                String []parseLine = line.split("::");
-                listHeadQuotes.add(new Quotes(parseLine[0]));
+        for (String line : readFile.getLine()) {
+            String[] parseLine = line.split("::");
+            Quotes who = new Quotes(parseLine[0]);
+            if(!listHeadQuotes.search(who)){
+                listHeadQuotes.add(who);
+            }
         }
 
         ListQuotes listQuotes1 = listHeadQuotes.getHead();
-        while (listQuotes1!=null){
-            System.out.println(listQuotes1.getQuotes()+"\n");
-            listQuotes1=listQuotes1.getNext();
+        while (listQuotes1 != null) {
+            System.out.println(listQuotes1.getQuotes() + "\n");
+            listQuotes1 = listQuotes1.getNext();
         }
+        System.out.println(listHeadQuotes.getSize());
     }
 }
